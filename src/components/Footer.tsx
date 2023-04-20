1c529fc41cc61bf95b7a899d136b452f8caaa493
Lucas Barbosa <soares_lucas13@hotmail.com>
2023-04-19 22:18:08 -0300

    Added Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,26 +4,55 @@ import { FaTwitter } from 'react-icons/fa';
 
 function Footer(): JSX.Element {
     return (
-        <footer className="bg-blue-600 text-white py-10 px-10 grid grid-cols-2 space-x-20 justify-between items-center w-full md:space-x-64">
+        <footer className="text-gray-600 py-5 px-5 md:py-10 md:px-10 lg:py-10 lg:px-10  gap-10 flex flex-row  justify-center items-center w-auto max-w-max border-t-2 mt-auto">
 
-            <div className="grid w-full">
+            <div className="flex flex-row md:w-full sm:w-full lg:w-full sm:gap-10 md:gap-14 lg:gap-24">
                 <div>
-                    <h1 className="font-medium text-xl">TRIP<span className='text-black'>valley</span>.</h1>
-                </div>
-                <div>
-                    <p>&copy; {new Date().getFullYear()} Trip Valley. Todos os direitos reservados.</p>
+                    <h1 className="lg:text-3xl font-black text-blue-600">TRIP<span className='text-gray-500'>valley</span>.</h1>
                 </div>
             </div>
-            <div className="grid grid-cols-3  ">
-                <a href="https://instagram.com" target={'_blank'}>
-                    <FaInstagram className='text-white text-2xl hover:text-black transition duration-300 ' />
-                </a>
-                <a href="https://facebook.com" target={'_blank'}>
-                    <FaFacebookF className='text-white text-2xl hover:text-black transition duration-300 ' />
-                </a>
-                <a href="https://facebook.com" target={'_blank'}>
-                    <FaTwitter className='text-white text-2xl hover:text-black transition duration-300 ' />
-                </a>
+            <div className='flex w-auto  gap-5 sm:gap-24  md:gap-32 lg:gap-52'>
+
+                {/* TripValley infos */}
+                <div className='sm:w-max md:w-max lg:w-max'>
+                    <div className='mb-2'>
+                        <h3 className='text-gray-800 font-semibold align-top'>TRIPvalley</h3>
+                    </div>
+                    <div className='space-y-2 cursor-pointer'>
+                        <h4 className='text-sm'>Sobre nós</h4>
+                        <h4 className='text-sm'>Seja um parceiro</h4>
+                        <h4 className='text-sm'>Trabalhe conosco</h4>
+                    </div>
+                </div>
+                {/* Contact */}
+                <div className='sm:w-max md:w-max lg:w-max'>
+                    <div className='mb-2'>
+                        <h3 className='text-gray-800 font-semibold align-top'>Central de atendimento</h3>
+                    </div>
+                    <div className='space-y-2 cursor-pointer'>
+                        <h4 className='text-sm'>(12) 9999-9999</h4>
+                        <h4 className='text-sm'>(12) 9999-9999</h4>
+                        <h4 className='text-sm'>contato@tripvalley.com.br</h4>
+                    </div>
+                </div>
+
+                {/* socials */}
+                <div className='sm:w-max md:w-max w-max displ lg:block md:block hidden'>
+                    <div className='mb-2'>
+                        <h3 className='text-gray-800 font-semibold'>Siga nossas redes</h3>
+                    </div>
+                    <div className="grid grid-cols-3 w-32">
+                        <a href="https://instagram.com" target={'_blank'}>
+                            <FaInstagram className='text-gray-800 text-2xl hover:text-blue-600 transition duration-300 ' />
+                        </a>
+                        <a href="https://facebook.com" target={'_blank'}>
+                            <FaFacebookF className='text-gray-800 text-2xl hover:text-blue-600 transition duration-300 ' />
+                        </a>
+                        <a href="https://facebook.com" target={'_blank'}>
+                            <FaTwitter className='text-gray-800 text-2xl hover:text-blue-600 transition duration-300 ' />
+                        </a>
+                    </div>
+                </div>
             </div>
         </footer>
     );
