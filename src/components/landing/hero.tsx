6dfedb911220c0a98b3e08795f64a3ebb280ee30
Marcelo Augusto <59924047+marceloarc@users.noreply.github.com>
2023-04-26 16:05:37 -0300

    Corrigido banner não preenchendo toda tela

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -2,7 +2,7 @@ export function Hero(): JSX.Element {
     return (
         <div className="hidden md:block lg:block">
             <div className="filter brightness-90">
-                <img className="w-auto h-min object-fill" src="/image/backgroundLd.png" alt="" />
+                <img className="lg:w-full h-min object-fill" src="/image/backgroundLd.png" alt="" />
             </div>
             <div className="absolute left-10 top-36 font-bold md:text-3xl lg:text-5xl md:max-w-xl lg:max-w-3xl text-white z-10">
                 <h1 className="text-gray-50 w-auto">Defina sua próxima <span className="text-yellow-400">viagem</span> de maneira simples e fácil</h1>
